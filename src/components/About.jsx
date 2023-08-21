aefda616c1c04c43416d7fd06c98d45ea7dbbbfd
windrianto3 <wulang.indrianto@gmail.com>
2023-08-21 19:18:06 -0400

    Add about info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
             <p className='text-xl pt-4'>This machine learning model classifies an uploaded mp3 file into one of ten classes: </p>
             <p className='text-xl py-2 font-mono'>Air Conditioner, Car Horn, Children Playing, Dog Bark, Drilling, Engine Idling, Gun Shot, Jackhammer, Siren, Street Music.</p>
             <p className='text-xl py-2'> The model is trained on the <a className='text-blue-700 cursor-pointer' href='https://urbansounddataset.weebly.com/urbansound8k.html'>UrbanSound8k</a> dataset, with  the <b>Keras/Tensorflow</b> libraries being used to train the model.</p>
-            <p className='text-xl py-2'> Uploaded files are sent to a <b>Flask API</b> that processes the sound file using <b>Librosa</b> to extract features, which are then passed through the model to predict the class.</p>
+            <p className='text-xl py-2'> Uploaded files are sent to a <b>Flask API</b> hosted on the <b>Google Cloud Platform</b> that processes the sound file using <b>Librosa</b> to extract features, which are then passed through the model to predict the class.</p>
             <p className='text-xl py-2'>For more information on the model, view the detailed report of the classification abilities on this <a className='text-blue-700 cursor-pointer' href='https://windrianto3.github.io/ml-final-report/'>Github page</a>.</p>
         </div>
         
